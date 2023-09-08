f95e83577ef3ec843e4cc4fb0fe6ce197d6a8261
yeaungzaw <yeaungzaw.learner@gmail.com>
2023-09-08 23:19:54 +0630

    update

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'heebo': ["Heebo", "Padauk"],
+        'nunito': ["Nunito", "Padauk"],
+      },
+      colors: {
+        'primary': "#0bbbcc",
+        'primary-hover': "#2BCAE0",
+        'secondary': "#F0FBFC",
+        'text-color': "#181D38",
+        'input-hover': "#14455D",
+      }
+    },
+  },
+  plugins: [],
+}
+
