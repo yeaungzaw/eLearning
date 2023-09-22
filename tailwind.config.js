6685a529e3c9fe973c94a228b2ef2d2b27b03ac1
yeaungzaw <yeaungzaw.learner@gmail.com>
2023-09-23 04:07:03 +0630

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,101 @@ module.exports = {
         'text-color': "#181D38",
         'text-color-2': "#52565b",
         'background-img-color': 'rgba(24, 29, 56, .7)',
+      },
+      keyframes: {
+        fadeInDown: {
+          'from': {
+            transform: 'rotateX(-75deg)',
+            opacity: '0'
+          },
+          'to': {
+            transform: 'rotateX(0deg)',
+            opacity: '1'
+          }
+        },
+
+        fadeUpDown: {
+          'from': {
+            transform: 'rotateX(0deg)',
+            opacity: '1'
+          }, 
+          'to': {
+            transform: 'rotateX(-75deg)',
+            opacity: '0'
+          }
+        },
+
+        slideInDown: {
+          'from': {
+            transform: 'translateY(-100%)',
+            opacity: '0'
+          },
+          'to': {
+            transform: 'translateY(0)',
+            opacity: '1'
+          }
+        },
+
+        slideInLeft: {
+          'from': {
+            transform: 'translateX(-100%)',
+            opacity: '0'
+          },
+          'to': {
+            transform: 'translateX(0)',
+            opacity: '1'
+          }
+        },
+
+        slideInRight: {
+          'from': {
+            transform: 'translateX(100%)',
+            opacity: '0'
+          },
+          'to': {
+            transform: 'translateX(0)',
+            opacity: '1'
+          }
+        },
+
+        slideInUp: {
+          'from': {
+            transform: 'translateY(100%)',
+            opacity: '0'
+          },
+          'to': {
+            transform: 'translateY(0)',
+            opacity: '1'
+          }
+        },
+
+        zoomIn: {
+          '0%': {
+            transform: 'scale(.3)',
+            opacity: '0'
+          },
+          '50%': {
+            opacity: '1'
+          }
+        },
+
+        fadeIn: {
+          '0%': {
+            opacity: '0'
+          },
+          '100%': {
+            opacity: '1'
+          }
+        }
+      },
+      animation: {
+        fadeInDown: "fadeInDown .5s ease-in",
+        fadeUpDown: "fadeUpDown .5s ease-in",
+        slideInDown: "slideInDown 1s ease-in",
+        slideInLeft: "slideInLeft 1s ease-in",
+        slideInRight: "slideInRight 1s ease-in",
+        zoomIn: "zoomIn .3s ease-in",
+        fadeIn: "fadeIn .5s ease-in"
       }
     },
   },
